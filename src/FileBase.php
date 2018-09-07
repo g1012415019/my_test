86d606a4bbb61c1c4c9e74dccb93f3e4170b1c14
gongzhe <1012415019@qq.com>
2018-09-07 14:17:15 +0800

    init

---
+++ src/FileBase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gongzhe\FileBase;
+
+/**
+ * 文件类
+ * @author gongzhe
+ * @createTime 2018-06-28 13:41:25
+ * @qqNumber 1012415019
+ * Class File
+ * @package app\admin\controller
+ */
+class FileBase
+{
+    /**
+     * 上传图片
+     * type 1 单图上传  默认单图
+     * type 2 多图上传
+     * module 所属模块  默认默认相册
+     * @author gongzhe
+     * @createTime 2018-06-28 13:42:28
+     * @qqNumber 1012415019
+     */
+    protected function index(){
+
+        phpinfo(); die;
+    }
+
+}
